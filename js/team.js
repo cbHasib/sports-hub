e41d905ac685dfb1c87e87fcb5d1bfd98ad4bc1b
cbHasib <hasibul.pbn@gmail.com>
2022-08-29 17:18:51 +0600

    Added popup player details

--- js/team.js
+++ js/team.js
@@ -41,18 +41,50 @@ const displayTeam = (teams) => {
         const div = document.createElement('div');
         div.classList.add('card', 'card-compact', 'w-full', 'h-full', 'bg-base-100', 'shadow-lg', 'text-center');
         div.innerHTML = `
-        <figure><img class="w-full" src="${strSportThumb}" alt="${strSport}" /></figure>
+        <figure><img class="w-full" src="${strSportThumb}" alt="${strSport}" onerror="this.onerror=null;this.src='https://upload.wikimedia.org/wikipedia/commons/thumb/d/d1/Image_not_available.png/640px-Image_not_available.png';"/></figure>
         <div class="card-body">
             <h2 class="card-title justify-center">${strSport}</h2>
             <p>${strSportDescription.slice(0, 90)}</p>
-            <div class="card-actions justify-center">
-                <button class="btn btn-primary" onclick="console.log('${idSport}')">Details</button>
+            <div class="card-actions justify-center">   
             </div>
         </div>
     `;
     teamContainer.appendChild(div);
     })
 }
+
+const modalData = (playerId) => {
+    const modalContainer = document.getElementById('modal-body');
+    modalContainer.innerHTML = '';
+    fetch(`https://www.thesportsdb.com/api/v1/json/2/lookupplayer.php?id=${playerId}`)
+    .then(res => res.json())
+    .then(data => modalBody(data.players[0]))
+}
+
+const modalBody = player => {
+    const {dateBorn, strPlayer, strThumb, strSport} = player;
+    // console.log(dateBorn, strPlayer, strSport);
+
+    const modalContainer = document.getElementById('modal-body');
+    const div = document.createElement('div');
+    // div.classList.add('modal-box');
+    div.innerHTML = `<div class="modal-box card w-full bg-base-100 shadow-xl">
+    <figure class="px-5 pt-5">
+    <img class="rounded-xl h-60 w-full object-cover" src="${strThumb}" alt="${strPlayer}" onerror="this.onerror=null;this.src='https://upload.wikimedia.org/wikipedia/commons/thumb/d/d1/Image_not_available.png/640px-Image_not_available.png';"/>
+    </figure>
+    <div class="card-body items-center text-center">
+        <h2 class="card-title">${strPlayer}</h2>
+        <p>${strSport}</p>
+        <p>${dateBorn}</p>
+        <div class="card-actions">
+        <label for="my-modal" class="btn btn-primary mt-2">Close</label>
+        </div>
+    </div>
+    </div>`;
+    modalContainer.appendChild(div);
+}
+
+
 const displaySearch = (players) => {
     const searchContainer = document.getElementById('search-container');
     searchContainer.innerHTML = ``;
@@ -64,16 +96,16 @@ const displaySearch = (players) => {
     // mainBody.style.display = 'block';
     // progress.style.display = 'none';
     players.forEach(player => {
-        const {strPlayer, strThumb, strDescriptionEN} = player;
+        const {strPlayer, strThumb, strDescriptionEN, idPlayer} = player;
         const div = document.createElement('div');
         div.classList.add('card', 'card-compact', 'w-full', 'h-full', 'bg-base-100', 'shadow-lg', 'text-center');
         div.innerHTML = `
-        <figure><img class="w-full" src="${strThumb}" alt="${strPlayer}" /></figure>
+        <figure><img class="w-full" src="${strThumb}" alt="${strPlayer}" onerror="this.onerror=null;this.src='https://upload.wikimedia.org/wikipedia/commons/thumb/d/d1/Image_not_available.png/640px-Image_not_available.png';"/></figure>
         <div class="card-body">
             <h2 class="card-title justify-center">${strPlayer}</h2>
             <p>${strDescriptionEN? (strDescriptionEN.slice(0, 90)) : "No Description Found"}</p>
             <div class="card-actions justify-center">
-                <button class="btn btn-primary">Details</button>
+            <label for="my-modal" class="btn btn-primary" onclick="modalData('${idPlayer}')">Details</label>
             </div>
         </div>
     `;
